6173d3b4a8c9c5e7c2c1b42dec4f4cfe0876da6e
Leivysallia <85811527+Leivysallia@users.noreply.github.com>
2024-01-18 19:39:12 -0500

    .

--- push.ps1
+++ push.ps1
@@ -4,16 +4,16 @@ set-location 'S:\Codex\OneDrive\GitHub\wishlist\'
 
 $unixtime = [DateTimeOffset]::Now.ToUnixTimeSeconds()
 
-if (!(test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\.bkup'))
+if (!(test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\bkup'))
 {
-    New-Item -Path 'S:\Codex\OneDrive\GitHub\wishlist\' -Name '.bkup\' -ItemType 'Directory'
+    New-Item -Path 'S:\Codex\OneDrive\GitHub\wishlist\' -Name 'bkup\' -ItemType 'Directory'
 }
 
 if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json')
 {
     if (test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json')
     {
-        move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json' -Destination "S:\Codex\OneDrive\GitHub\wishlist\.bkup\$unixtime.json"
+        move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json' -Destination "S:\Codex\OneDrive\GitHub\wishlist\bkup\$unixtime.json"
     }
     move-item -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json' -Destination 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json'
 }
@@ -22,16 +22,16 @@ if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....txt')
 {
     if (test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt')
     {
-        move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt' -Destination "S:\Codex\OneDrive\GitHub\wishlist\.bkup\$unixtime.txt"
+        move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt' -Destination "S:\Codex\OneDrive\GitHub\wishlist\bkup\$unixtime.txt"
     }
     move-item -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....txt' -Destination 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt'
 }
 
-7za u -r -y -mx9 .bkup.zip .bkup
+7za u -r -y -mx9 bkup.zip bkup
 
 start-sleep 1
 
-Remove-Item '.bkup\' -r
+Remove-Item -LiteralPath ".\bkup" -Recurse
 
 start-sleep 1
 
