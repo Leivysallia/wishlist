54b122c20ce13b7f017cb2d493e90e68457aadc7
Leivysallia <85811527+Leivysallia@users.noreply.github.com>
2024-01-18 19:36:03 -0500

    .

--- push.ps1
+++ push.ps1
@@ -4,6 +4,11 @@ set-location 'S:\Codex\OneDrive\GitHub\wishlist\'
 
 $unixtime = [DateTimeOffset]::Now.ToUnixTimeSeconds()
 
+if (!(test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\.bkup'))
+{
+    New-Item -Path 'S:\Codex\OneDrive\GitHub\wishlist\' -Name '.bkup\' -ItemType 'Directory'
+}
+
 if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json')
 {
     if (test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json')
@@ -12,7 +17,6 @@ if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json')
     }
     move-item -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json' -Destination 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json'
 }
-
 
 if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....txt')
 {
@@ -23,7 +27,14 @@ if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....txt')
     move-item -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....txt' -Destination 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt'
 }
 
+7za u -r -y -mx9 .bkup.zip .bkup
+
+start-sleep 1
+
+Remove-Item '.bkup\' -r
+
+start-sleep 1
+
 git commit -am.
 
 git push
-
