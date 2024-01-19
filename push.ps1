906042b32d068e0d586d53511c15c5725a253450
Leivysallia <85811527+Leivysallia@users.noreply.github.com>
2024-01-18 19:06:28 -0500

    committed

--- push.ps1
+++ push.ps1
@@ -1,23 +1,21 @@
 clear-host
 
 $unixtime = [DateTimeOffset]::Now.ToUnixTimeSeconds()
-
-if (test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json')
+if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json')
 {
-    move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json' -Destination "S:\Codex\OneDrive\GitHub\wishlist\bkup\$unixtime.json"
+    if (test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json')
+    {
+        move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json' -Destination "S:\Codex\OneDrive\GitHub\wishlist\.bkup\$unixtime.json"
+    }
+    move-item -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json' -Destination 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json'
 }
 
-if (test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt')
-{
-    move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt' -Destination "S:\Codex\OneDrive\GitHub\wishlist\bkup\$unixtime.txt"
-}
 
 if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....txt')
 {
+    if (test-path -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt')
+    {
+        move-item -LiteralPath 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt' -Destination "S:\Codex\OneDrive\GitHub\wishlist\.bkup\$unixtime.txt"
+    }
     move-item -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....txt' -Destination 'S:\Codex\OneDrive\GitHub\wishlist\elisys.txt'
-}
-
-if (test-path -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json')
-{
-    move-item -LiteralPath 'S:\Codex\OneDrive\Downloads\Uhh....json' -Destination 'S:\Codex\OneDrive\GitHub\wishlist\elisys.json'
 }
