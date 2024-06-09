5c8c393ba4efe5465b3a0ed657d82f6f47c81ca0
Leivysallia <aelenydd@gmail.com>
2024-06-09 00:12:53 -0400

    .

--- push.ps1
+++ push.ps1
@@ -1,17 +1,17 @@
 clear-host
 
-set-location 'S:\Codex\Drive\GitHub\wishlist\'
+set-location 'S:\Codex\OneDrive\Documents\GitHub\wishlist\'
 
 $unixtime = [DateTimeOffset]::Now.ToUnixTimeSeconds()
 
-New-Item -Path 'S:\Codex\Drive\GitHub\wishlist\bkup'  -ItemType 'Directory' -force
+New-Item -Path 'S:\Codex\OneDrive\Documents\GitHub\wishlist\bkup'  -ItemType 'Directory' -force
 
-if (test-path 'S:\Codex\Drive\GitHub\wishlist\elisys.json') {
-    Copy-Item 'S:\Codex\Drive\GitHub\wishlist\elisys.json' "S:\Codex\Drive\GitHub\wishlist\bkup\elisys.json.bkup"
+if (test-path 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.json') {
+    Copy-Item 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.json' "S:\Codex\OneDrive\Documents\GitHub\wishlist\bkup\elisys.json.bkup"
 }
 
-if (test-path 'S:\Codex\Drive\GitHub\wishlist\elisys.txt') {
-    Copy-Item 'S:\Codex\Drive\GitHub\wishlist\elisys.txt' "S:\Codex\Drive\GitHub\wishlist\bkup\elisys.txt.bkup"
+if (test-path 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.txt') {
+    Copy-Item 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.txt' "S:\Codex\OneDrive\Documents\GitHub\wishlist\bkup\elisys.txt.bkup"
 }
 
 zip -0rm '.\bkup.zip' '.\bkup\'
