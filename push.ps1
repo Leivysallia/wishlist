fc174c1ffde93a0922e30a26c549c856b79ee561
Leivysallia <aelenydd@gmail.com>
2024-06-15 08:49:50 -0400

    .

--- push.ps1
+++ push.ps1
@@ -4,20 +4,6 @@ clear-host
 
 set-location 'S:\Codex\OneDrive\Documents\GitHub\wishlist\'
 
-#$unixtime = [DateTimeOffset]::Now.ToUnixTimeSeconds()
-
-New-Item -Path 'S:\Codex\OneDrive\Documents\GitHub\wishlist\bkup'  -ItemType 'Directory' -force
-
-if (test-path 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.json') {
-    Copy-Item 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.json' "S:\Codex\OneDrive\Documents\GitHub\wishlist\bkup\elisys.json.bkup"
-}
-
-if (test-path 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.txt') {
-    Copy-Item 'S:\Codex\OneDrive\Documents\GitHub\wishlist\elisys.txt' "S:\Codex\OneDrive\Documents\GitHub\wishlist\bkup\elisys.txt.bkup"
-}
-
-zip -0rm '.\bkup.zip' '.\bkup\'
-
 git commit -am.
 
 git push
