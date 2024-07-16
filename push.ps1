4a051ce4068fdbb46e1c356ac2f02e38564e14de
Leivysallia <aelenydd@gmail.com>
2024-07-15 21:28:24 -0400

    push.ps1

--- push.ps1
+++ push.ps1
@@ -1,11 +1,20 @@
 Clear-Host
-
+Set-Location $PSScriptRoot
 #https://raw.githubusercontent.com/Leivysallia/wishlist/main/echos.txt
 #https://raw.githubusercontent.com/Leivysallia/wishlist/main/leivysallia.txt
 #https://raw.githubusercontent.com/Leivysallia/wishlist/main/elisys.txt
 
-Set-Location $PSScriptRoot
+#git config --global user.email "aelenydd@gmail.com"
+#git config --global credential.helper wincred
+#git config --global user.name "Leivysallia"
+#git lfs install
+#git lfs track "*.wim"
+#git lfs track "*.zip"
+#git lfs track "*.7z"
+#git lfs track "*.exe"
+#git lfs track "*.dll"
+#git add .gitattributes
 
-git commit -am.
-
+git add --all
+git commit -am'push.ps1'
 git push
