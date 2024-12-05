ee7136e22aaeae59f091bc09589ccda1c8ad058e
Leivysallia <85811527+Leivysallia@users.noreply.github.com>
2024-12-05 12:47:23 -0500

    .

--- pretifyjson.js
+++ pretifyjson.js
@@ -3,6 +3,6 @@ import fs from 'fs-extra';
 const buffer = await fs.readFile('./deliverables/bundles/revia-seasons.json');
 let yourJsonText = buffer.toString();
 let yourJson = JSON.parse(yourJsonText);
-let bob = JSON.stringify(yourJson, null, 4);
+let reviaScarlet = JSON.stringify(yourJson, null, 4);
 
-console.log(bob);
+fs.writeFile('Revia Scarlet.json', reviaScarlet);
