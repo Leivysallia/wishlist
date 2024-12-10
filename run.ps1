392d926a921c09156c10db1207fe3ddcb0e88e74
Leivysallia <85811527+Leivysallia@users.noreply.github.com>
2024-12-09 21:59:43 -0500

    .

--- run.ps1
+++ run.ps1
@@ -1,12 +1,14 @@
-clear-host
-Set-Location 'C:\Users\cstat\onedrive\documents\GitHub\wishlist'
+if ($false) {
+    Clear-Host
+    Set-Location 'C:\Users\cstat\onedrive\documents\GitHub\wishlist'
 
-Copy-Item .\.scripts\bundler\* . -Force
+    Copy-Item .\.scripts\bundler\* . -Force
 
-node 'index.js'
-node 'pretifyjson.js' > '.\Revia Scarlet.json'
+    node 'index.js'
+    node 'pretifyjson.js' > '.\Revia Scarlet.json'
 
-Start-Sleep 1
+    Start-Sleep 1
 
-Remove-Item 'index.js'
-Remove-Item 'typedefs.js'
+    Remove-Item 'index.js'
+    Remove-Item 'typedefs.js'
+}
